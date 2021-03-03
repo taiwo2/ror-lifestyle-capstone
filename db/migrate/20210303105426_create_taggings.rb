1b75b79b67c0f0229a98bb9eada8c74e1e660f66
taiwo2 <adetonat@ymail.com>
2021-03-03 12:22:01 +0100

    Migrate database

--- db/migrate/20210303105426_create_taggings.rb
+++ db/migrate/20210303105426_create_taggings.rb
@@ -1,7 +1,7 @@
 class CreateTaggings < ActiveRecord::Migration[6.1]
   def change
     create_table :taggings do |t|
-      t.references :Tag, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
       t.references :article, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20210303110000_add_columns_to_users.rb
+++ db/migrate/20210303110000_add_columns_to_users.rb
@@ -3,7 +3,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[6.1]
     add_column :users, :twitter, :string
     add_column :users, :facebook, :string
     add_column :users, :linkedin, :string
-    add_column :users, :avatar, :string
     add_column :users, :profile_image, :string
     add_column :users, :admin, :boolean, default: false
   end
